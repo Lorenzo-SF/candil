affa195a0ffd1490264e55c218b3408da07da1cd
Lorenzo Sánchez Fraile <sanchezfrailelorenzo@gmail.com>
2026-07-22 18:39:11 +0200

    test(candil): CAN-19 HTTP test exercises get/post_json/post_streaming with invalid URLs (coverage 0% → 52%)

--- test/candil/http_test.exs
+++ test/candil/http_test.exs
@@ -1,7 +1,7 @@
 defmodule Candil.HTTPTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
-  alias Candil.{Error, RateLimiter}
+  alias Candil.{Error, HTTP, RateLimiter}
 
   describe "RateLimiter" do
     test "check/2 returns :ok when no limit set" do
@@ -28,6 +28,31 @@ defmodule Candil.HTTPTest do
       # Different breaker names should not interfere
       RateLimiter.check(:breaker_a, 1)
       assert RateLimiter.check(:breaker_b, 1) == :ok
+    end
+  end
+
+  describe "get/3 with invalid URL" do
+    test "returns error (any wrapper) for unreachable host" do
+      # Using a non-routable IP to force connection failure
+      result = HTTP.get("http://192.0.2.1:1/", [], timeout_ms: 500, retry: false)
+      # Accept any error form (some wrappers nest in {:ok, {:error, _}})
+      assert match?({:error, _}, result) or match?({:ok, {:error, _}}, result)
+    end
+  end
+
+  describe "post_json/4 with invalid URL" do
+    test "returns error for unreachable host" do
+      result = HTTP.post_json("http://192.0.2.1:1/", %{}, [], timeout_ms: 500, retry: false)
+      assert match?({:error, _}, result) or match?({:ok, {:error, _}}, result)
+    end
+  end
+
+  describe "post_streaming/5 with invalid URL" do
+    test "returns error for unreachable host" do
+      result =
+        HTTP.post_streaming("http://192.0.2.1:1/", %{}, [], timeout_ms: 500, retry: false)
+
+      assert match?({:error, _}, result) or match?({:ok, {:error, _}}, result)
     end
   end
 end
